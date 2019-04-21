68861a87528986d914fc96fc060a34bcfa0cf309
Mo <hayekmohaned2@gmail.com>
2019-04-20 20:55:03 -0600

    rat maze added

--- TextureDemo2012/graphics1.cpp
+++ TextureDemo2012/graphics1.cpp
@@ -10,16 +10,79 @@
 #include <iostream>
 using namespace std;
 #include "graphics.h"
-
+#include <ctime>
+#include "rat.h"
+#include "maze.h"
 
 // Global Variables (Only what you need!)
 double screen_x = 700;
 double screen_y = 500;
+bool gMouseLeft = false;
+bool gMouseMiddle = false;
+bool gMouseRight = false;
+enum viewtype { top_view, perspective_view, rat_view };
+viewtype current_view = perspective_view;
+int mstart;
+int mendx;
+int mendy;
+int mM;
+int mN;
+bool top = true;
+bool bottom = true;
+bool left = true;
+bool right = true;
+Maze gMaze;
+Rat mRat;
 
 // Textures
 const int num_textures = 4;
 static GLuint texName[num_textures];
+/*Helper Functions*/
 
+double GetTime() {
+	static clock_t start_time = clock();
+	clock_t end_time = clock();
+	return (double)(end_time - start_time) / CLOCKS_PER_SEC;
+}
+double getDeltaTime() {
+	static clock_t starttime = clock();//built in function clock() problem default is 17 clicks a second while we need about 100 so we made changes
+	static int currentframe = 0;
+	clock_t currenttime = clock();
+	currentframe += 1;
+	double totaltime = double(currenttime - starttime) / CLOCKS_PER_SEC;//CLOCKS_PER_SEC is a built in function
+	if (totaltime == 0)//prevents dividing by zero
+	{
+		totaltime = .0001;
+	}
+	//cout << "current frame" << currentframe << endl;
+	double framespersecond = (double)currentframe / totaltime;
+	double DT = 1.0 / framespersecond;
+	return DT;
+
+}
+void SetPerspectiveView(int w, int h)
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double aspectRatio = (GLdouble)w / (GLdouble)h;
+	gluPerspective(
+		/* field of view in degree */ 38.0,
+		/* aspect ratio */ aspectRatio,
+		/* Z near */ .1, /* Z far */ 30.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void SetTopView(int w, int h)
+{
+	// go into 2D mode
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	double world_margin_x = 0.5;
+	double world_margin_y = 0.5;
+	gluOrtho2D(-world_margin_x, M + world_margin_x,
+		-world_margin_y, N + world_margin_y);
+	glMatrixMode(GL_MODELVIEW);
+}
 // tga image loader code.
 #include "tga.h"
 gliGenericImage *readTgaImage(char *filename)
@@ -161,22 +224,64 @@ void DrawText(double x, double y, char *string)
 // GLUT callback functions
 //
 
+
 // This callback function gets called by the Glut
 // system whenever it decides things need to be redrawn.
 void display(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3d(1,1,1);
+	//glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor3d(0,0,0); //forground color
 
-	glLoadIdentity();
+	double eye[3] = { -3, -3, 7 }; // pick a nice vantage point.
+	double at[3] = { 3, 3, 0 };
 
-	double eye[3] = {5, -1, 11}; // pick a nice vantage point.
-	double at[3]  = {5, 3, 0};
+	
+	if (current_view == perspective_view)
+	{
+		glEnable(GL_DEPTH_TEST);
+		glLoadIdentity();
+		gluLookAt(eye[0], eye[1], eye[2], at[0], at[1], at[2], 0, 0, 1); // Z is up!
+	}
+	else if (current_view == top_view)
+	{
+		glDisable(GL_DEPTH_TEST);
+		glLoadIdentity();
+	}
+	else // current_view == rat_view
+	{
+		glEnable(GL_DEPTH_TEST);
+		glLoadIdentity();
+		double z_level = .25;
+		double x = mRat.getX();
+		double y = mRat.getY();
+		double dx = cos(mRat.getDirection() / 180 * 3.14);
+		double dy = sin(mRat.getDirection() / 180 * 3.14);
+		double at_x = x + dx;
+		double at_y = y + dy;
+		double at_z = z_level;
+		gluLookAt(x, y, z_level, at_x, at_y, at_z, 0, 0, 1);
+	}
+	//glLoadIdentity();
+	if (gMouseLeft) {
+		mRat.spinLeft();
+	}
 
-	gluLookAt(eye[0], eye[1], eye[2],  at[0], at[1], at[2],  0,0,1); // Z is up!
+	if (gMouseRight) {
+		mRat.spinRight();
+	}
+
+	if (gMouseMiddle) {
+		mRat.move();
+	}
+
+	gMaze.Draw();
+
+	mRat.Draw();
 
 	// Draw quads with texture
 	glEnable(GL_TEXTURE_2D);
+
 	glBindTexture(GL_TEXTURE_2D, texName[0]);
 
 	glBegin(GL_QUADS);
@@ -204,6 +309,38 @@ void keyboard(unsigned char c, int x, int y)
 		case 'b':
 			// do something when 'b' character is hit.
 			break;
+		case 't':
+			current_view = top_view;
+			SetTopView(screen_x, screen_y);
+			/*Remove or construct top wall of the current cell */
+			// if (top){
+			// 	gMaze.removeTopWall(mRat.getX(), mRat.getY(), false);
+			// 	mRat.setMaze(&gMaze);
+			// 	top = false;
+			// } else {
+			// 	gMaze.removeTopWall(mRat.getX(), mRat.getY(), true);
+			// 	mRat.setMaze(&gMaze);
+			// 	top = true;
+			// }
+			break;
+		case 'r':
+			current_view = rat_view;
+			SetPerspectiveView(screen_x, screen_y);
+			break;
+		case 'p':
+			/*When the user clicks ‘p’, see the world from a perspective viewpoint of your choosing.*/
+			current_view = perspective_view;
+			SetPerspectiveView(screen_x, screen_y);
+			break;
+		case 'w':
+			mRat.move();
+			break;
+		case 'a':
+			mRat.spinLeft();
+			break;
+		case 'd':
+			mRat.spinRight();
+			break;
 		default:
 			return; // if we don't care, return without glutPostRedisplay()
 	}
@@ -212,17 +349,7 @@ void keyboard(unsigned char c, int x, int y)
 }
 
 
-void SetPerspectiveView(int w, int h)
-{
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	double aspectRatio = (double) w/(double) h;
-	gluPerspective( 
-	/* field of view in degree */ 38.0,
-	/* aspect ratio */ aspectRatio,
-	/* Z near */ 1, /* Z far */ 1000);
-	glMatrixMode(GL_MODELVIEW);
-}
+
 
 // This callback function gets called by the Glut
 // system whenever the window is resized by the user.
@@ -242,17 +369,29 @@ void reshape(int w, int h)
 // system whenever any mouse button goes up or down.
 void mouse(int mouse_button, int state, int x, int y)
 {
-	if (mouse_button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) 
+	if (mouse_button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
+		gMouseLeft = true;
 	}
-	if (mouse_button == GLUT_LEFT_BUTTON && state == GLUT_UP) 
+	if (mouse_button == GLUT_LEFT_BUTTON && state == GLUT_UP)
 	{
+		gMouseLeft = false;
 	}
-	if (mouse_button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) 
+	if (mouse_button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
 	{
+		gMouseMiddle = true;
 	}
-	if (mouse_button == GLUT_MIDDLE_BUTTON && state == GLUT_UP) 
+	if (mouse_button == GLUT_MIDDLE_BUTTON && state == GLUT_UP)
 	{
+		gMouseMiddle = false;
+	}
+	if (mouse_button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+	{
+		gMouseRight = true;
+	}
+	if (mouse_button == GLUT_RIGHT_BUTTON && state == GLUT_UP)
+	{
+		gMouseRight = false;
 	}
 	glutPostRedisplay();
 }
@@ -270,12 +409,22 @@ bool PowerOf2(int h)
 // Your initialization code goes here.
 void InitializeMyStuff()
 {
+	gMaze.Init();
+	mRat.setMaze(&gMaze);
+	mstart = gMaze.getStart();
+	mendx = gMaze.getEndX();
+	mendy = gMaze.getEndY();
+	mM = gMaze.getM();
+	mN = gMaze.getN();
+	mRat.Init(double(mstart));
+
 	gliGenericImage *image[num_textures];
 	int n=0;
 	image[n++] = readImage("fruit.tga");
 	image[n++] = readImage("sky.tga");
 	image[n++] = readImage("cookies.tga");
 	image[n++] = readImage("Seattle.tga");
+	//glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	if(n!=num_textures)
 	{
 		printf("Error: Wrong number of textures\n");
@@ -363,7 +512,7 @@ int main(int argc, char **argv)
 	glutReshapeFunc(reshape);
 	glutMouseFunc(mouse);
 
-	glClearColor(.2, .5, .1, 0);	
+	glClearColor(.2, .5, .1, 0);	//background color
 	InitializeMyStuff();
 
 	glutMainLoop();
